a645d0f32de491cdc9c4940c0e43b886d775bce8
yghernan <102157602+saitanax3000@users.noreply.github.com>
2023-05-14 16:00:50 -0400

    admin settings
    Co-Authored-By: diegete <101644060+diegete@users.noreply.github.com>

--- productos_api/admin.py
+++ productos_api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from.models import *
 # Register your models here.
+
+class ProductosAdmin(admin.ModelAdmin):
+    list_display=('idProducto','nombre','categoria','estado','imagen','stock','precio')
+    
+
+admin.site.register(Producto,ProductosAdmin)
